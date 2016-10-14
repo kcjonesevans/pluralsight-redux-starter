d154a0922a9aefc96daf037cf7ffb2d806b2452e
K.C. Jones-Evans <kc.jones-evans@appliedis.com>
2016-10-14 14:43:26 -0400

    adding courses using state

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react';
-import connect from 'react-redux';
+import {connect} from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component{
@@ -20,14 +20,19 @@ class CoursesPage extends React.Component{
  }
 
  onClickSave(){
-   alert(`Saving ${this.state.course.title}`);
    this.props.dispatch(courseActions.createCourse(this.state.course));
  }
 
+ courseRow(course, index){
+   return <div key={index}>{course.title}</div>;
+ }
+
   render(){
+
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
         <input type="submit" value="Save" onClick={this.onClickSave} />
@@ -36,10 +41,16 @@ class CoursesPage extends React.Component{
   }
 }
 
-function mapStatetoProps(state, ownProps){
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps){
+  
   return {
     courses: state.courses
-  }
+  };
 }
 
-export default connect(mapStatetoProps)(CoursesPage);
+export default connect(mapStateToProps)(CoursesPage);
