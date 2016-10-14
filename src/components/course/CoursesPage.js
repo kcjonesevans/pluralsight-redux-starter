7482ccd2d1a9717d096719737be0c5236ef31377
K.C. Jones-Evans <kc.jones-evans@appliedis.com>
2016-10-14 14:49:42 -0400

    manual dispatch

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -20,7 +20,7 @@ class CoursesPage extends React.Component{
  }
 
  onClickSave(){
-   this.props.dispatch(courseActions.createCourse(this.state.course));
+   this.props.createCourse(this.state.course);
  }
 
  courseRow(course, index){
@@ -42,15 +42,21 @@ class CoursesPage extends React.Component{
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps){
-  
+
   return {
     courses: state.courses
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch){
+  return{
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
